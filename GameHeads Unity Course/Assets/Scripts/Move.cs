d43130b9fa66a88b954b36a5621aa3410375a3aa
BlackShadow103 <kt3sarone@gmail.com>
2020-04-25 15:34:39 -0700

    Unity Class Code 4.18.20
    Added the follow script to move the camera to follow the capsule

--- GameHeads Unity Course/Assets/Scripts/Move.cs
+++ GameHeads Unity Course/Assets/Scripts/Move.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    //Amount of units to move per second
     public Vector3 speed;
 
     // Start is called before the first frame update
@@ -15,7 +16,26 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 currentSpeed = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentSpeed.x = -speed.x;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            currentSpeed.x = speed.x;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currentSpeed.z = speed.z;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentSpeed.z = -speed.z;
+        }
+
         //Speed us a Vector3 defines how fast to move in 3D space
-        gameObject.transform.Translate(speed);
+        gameObject.transform.Translate(currentSpeed * Time.deltaTime);
     }
 }
